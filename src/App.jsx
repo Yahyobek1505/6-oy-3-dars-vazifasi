0f1cfac40e1fc7dbd088e4e8f6355614db1aa2df
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-02-14 07:57:18 +0500

    Homework is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState('');
 
-  function handleClick() {
-    setCounter(counter + 1)
-  }
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputText.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: inputText, completed: false }]);
+      setInputText('');
+    }
+  };
+
+  const handleToggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <>
-    <div className='mainPage'>
-      <div className='todoForm'> 
-       <form>
-        <input className='Todo-input' placeholder='Enter your tasks...' type="text" name="todo"/>
-        <button>Add</button>
-       </form>
+    <div className="MainPage">
+      <h1>Todo App</h1>
+      <div className="todoForm">
+        <input className='Todo-input'
+          type="text"
+          placeholder="Enter a todo..."
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
       </div>
+     <div className='result'>
+     <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+            <span onClick={() => handleToggleComplete(todo.id)}>{todo.text}</span>
+          </li>
+        ))}
+      </ul>
+     </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
